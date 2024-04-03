67933d25be0fc7ec71bbd9efd7f1e7724aa710f2
MyMegaUser <misha5220@yandex.ru>
2024-04-03 20:58:05 +0300

    edit

--- src/main/java/config/BaseTest.java
+++ src/main/java/config/BaseTest.java
@@ -1,8 +1,63 @@
 package config;
 
 
-public class BaseTest {
-    public static void main(String[] args) {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
+import java.time.Duration;
+
+public class BaseTest {
+
+    private WebDriver driver;
+
+    public WebDriver getDriver() {
+        return driver;
     }
+
+@BeforeMethod
+@Parameters("browser")
+    public void setUp(@Optional ("firefox") String browser){
+
+        if(browser.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--lang=en");
+
+            driver=new ChromeDriver(options);
+
+        } else if (browser.equalsIgnoreCase("firefox")){
+            WebDriverManager.firefoxdriver().setup();
+            FirefoxOptions options  = new FirefoxOptions();
+            options.addPreference("intl.accept_languages","en");
+            driver = new FirefoxDriver(options);
+
+        }else{
+            throw new IllegalArgumentException("invalid browser value" + browser);
+        }
+
+        driver = getDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+
+
+
+
+}
+
+@AfterMethod
+    public void tearDown(){
+       if(driver!=null) driver.quit();
+    }
+
+
 }
